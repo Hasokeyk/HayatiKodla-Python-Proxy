0853f26ad24dcf08232fb172bbfbab847aa166de
Hasan Yüksektepe <hasanhasokeyk@hotmail.com>
2018-01-06 15:40:53 +0300

    Url kontrol sistemi değişti
    Eski url validate çalışmıyordu. Urlparse ederek çözdüm

--- app.py
+++ app.py
@@ -6,26 +6,24 @@ from flask import abort
 import urllib
 import requests
 import re
+try:
+    from urllib.parse import urlparse
+except ImportError:
+    from urlparse import urlparse
 #from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
 def is_http_url(s):
-    
-    regex = re.compile(
-            r'^(?:http|ftp)s?://' # http:// or https://
-            r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' # domain...
-            r'localhost|' # localhost...
-            r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|' # ...or ipv4
-            r'\[?[A-F0-9]*:[A-F0-9:]+\]?)' # ...or ipv6
-            r'(?::\d+)?' # optional port
-            r'(?:/?|[/?]\S+)$', re.IGNORECASE)
-    
-    if re.match(regex,s):
-        return True
-    else:
+    try:
+        url = urlparse(s)
+        if url.scheme:
+            return True
+        else:
+            return False
+    except Exception as Hata:
         return False
-
+ 
 @app.route('/', methods=['GET','POST'])
 def index():
     if request.method == 'POST':
@@ -41,8 +39,8 @@ def index():
                     return text.text
                 else:
                     return "501 Sitede bağlantı sorunu var"
-            except:
-                return "500 Siteye Bağlanamadı. Linki kontrol edin"
+            except Exception as hata:
+                return "500 Siteye Bağlanamadı. Linki kontrol edin."
         else:
             return '502 Domain Geçerli Değil ÖRN. https://www.hayatikodla.net olarak gönderin'
     else:
