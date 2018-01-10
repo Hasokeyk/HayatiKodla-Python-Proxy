5aa45d7bdc2e81a5cb743e3480c40105128d6fc0
Hasan Yüksektepe <hasanhasokeyk@hotmail.com>
2018-01-10 16:46:55 +0300

    Mobil Sorgulama
    Mobil parametresi true ise mobil gibi sorgulama yapıyor

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #  -*- coding: utf-8 -*-
 
+#Kütüphanaler
 from flask import Flask
 from flask import request
 from flask import abort
@@ -11,6 +12,12 @@ try:
 except ImportError:
     from urlparse import urlparse
 #from bs4 import BeautifulSoup
+#Kütüphanaler
+
+#Değişkenler
+domain = None
+mobil  = None
+#Değişkenler
 
 app = Flask(__name__)
 
@@ -27,20 +34,53 @@ def is_http_url(s):
 @app.route('/', methods=['GET','POST'])
 def index():
     if request.method == 'POST':
-        domain = request.form['domain']
+        try:
+            if request.form['domain'] is not None:
+                domain = request.form['domain']
+            else:
+                domain = None
+        except:
+            return '503 Domain Parametresi Bulunamadı'
+        try:
+            if request.form['mobil'] is not None:   
+                type = request.form['mobil']
+            else:
+                type = None
+        except:
+            type = None 
     else:
-        domain = request.args.get('domain')
-        
+        try:
+            if request.args.get('domain') is not None:
+                domain = request.args.get('domain')
+            else:
+                domain = None
+        except:
+            return '503 Domain Parametresi Bulunamadı'
+        try:
+            if request.args.get('mobil') is not None:   
+                type = request.args.get('mobil')
+            else:
+                type = None
+        except:
+            type = None
+
+    if type is not None and type == 'true':
+        userAgent = 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Mobile Safari/537.36'
+    else:
+        userAgent = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'
+    
+    print(type,userAgent)
+    
     if domain is not None:
         if is_http_url(domain):
             try:
-                text = requests.get(domain)
+                text = requests.get(domain,headers={'User-agent':userAgent})
                 if text.status_code == 200:
                     return text.text
                 else:
                     return "501 Sitede bağlantı sorunu var. Status Code : "+text.status_code
             except Exception as hata:
-                return "500 Siteye Bağlanamadı. Linki kontrol edin."
+                return "500 Siteye Bağlanamadı. Linki kontrol edin."+text.status_code
         else:
             return "502 Domain Geçerli Değil ÖRN. https://www.hayatikodla.net olarak gönderin."
     else:
